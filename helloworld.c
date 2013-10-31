38dfcd12cea84a247bd539cf120d016a5d2688e3
ktran21 <ktran21@student.gsu.edu>
2013-10-30 23:53:16 -0400

    I apologize for being a chronic procrastinator!

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* Loads stdio library. To make this executable, must compile in terminal by using  gcc -o helloworld -ansi helloworld.c */
+main() {
+/* printf method used to output on screen and \n is for newline character */
+	printf("Hello World!\n");
+/* Must return something but since nothing to return use 0 */
+	return(0);
+}
