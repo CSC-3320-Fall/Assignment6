d87a0d8763f13d53577a36e4e5baf2ee03490d45
Robert French <frencrs@gmail.com>
2013-10-31 13:42:18 -0400

    I've always been told I have problems with commitment.

---
+++ rfrench8/helloworld.c
@@ -0,0 +1,26 @@
+/*
+Robert French
+
+
+hello.c in rwh dir. works by print adding up 'o' to:
+
+25960 27756 8303 28535 27762 2660
+
+which 
+
+0110010101101000 0110110001101100  001000001101111 etc...
+
+  101     104      108     108       32     111    etc...
+
+they are shifted then printed:
+
+104 101 108 108 111 32 119 111 114 108 100 10
+ h   e   l   l   o  \s  w   o   r   l   d  \n
+
+*/
+
+#include <stdio.h>
+int main(int argc, char *argv)
+{
+ printf("hello world\n");
+}
